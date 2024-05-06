75b49d62e143416c8720863d38e68b410cc3f74f
Edunka <emil.dunka96@gmail.com>
2024-05-06 13:57:39 +0100

    allow cross origin resource sharing

--- api-get.js
+++ api-get.js
@@ -1,17 +1,36 @@
+import express from 'express';
+import cors from 'cors';
 import axios from 'axios';
+const app = express();
+app.use(cors());
+
 const newsUrl = axios.create({
     baseURL: 'https://backend-nc-project.onrender.com/api',
 });
 
-const getArticles = () =>{
-    return newsUrl.get(`/articles`)
-    .then((res) =>{
-        return res.data;
-    })
-    .catch((err) =>{
-        console.error(err)
-        throw err;
-    })
-}
+export const getArticles = () => {
+    return newsUrl.get('/articles')
+        .then((response) => {
+            return response.data;
+        })
+        .catch((err) => {
+            console.error(err);
+            throw err;
+        });
+};
 
-export default getArticles
+app.get('/articles', (req, res) => {
+    getArticles()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            console.error("Error fetching articles:", err);
+            res.status(500).json({ error: "Failed to fetch articles" });
+        });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
